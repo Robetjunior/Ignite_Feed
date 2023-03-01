4dcc278151cd19fa4a0165be574870f7df54bd6c
Robetjunior <juniorferreirarob@gmail.com>
2023-02-28 21:48:20 -0300

    create Feed to post your comment about it

---
+++ src/App.tsx
@@ -0,0 +1,63 @@
+import { Header } from './components/Header'
+import { Post } from './components/Post'
+import { Sidebar } from './components/Sidebar'
+
+import styles from './App.module.css'
+import './global.css'
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https:github.com/Robetjunior.png',
+      name: 'José Roberto',
+      role: 'Engenheiro de Software',
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋' },
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2023-08-01 13:00:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/milremmirror.png',
+      name: 'Diogo de Souza',
+      role: 'Salesforce Developer'
+    },
+    content: [
+      {type: 'paragraph', content: 'Fala galeraa 👋' },
+      {type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀'},
+      {type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2023-05-03 13:00:00')
+  },
+]
+function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        
+        <main>
+          {posts.map(post => {
+            return (
+              <Post 
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
+        </main>
+      </div>
+    </div>
+  )
+}
+
+export default App
